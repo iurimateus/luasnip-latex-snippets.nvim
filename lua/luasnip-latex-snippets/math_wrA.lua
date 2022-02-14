f5e325cb6c675a6c7c04ae7d0a9571851c56433c
Iuri Mateus <20242530+iurimateus@users.noreply.github.com>
2022-02-14 14:44:22 -0300

    fix(math): move '() frac' bwA -> wrA

--- lua/luasnip-latex-snippets/math_wrA.lua
+++ lua/luasnip-latex-snippets/math_wrA.lua
@@ -14,7 +14,49 @@ local frac_no_parens = {
   i(0),
 }
 
+local frac = s({
+  priority = 1000,
+  trig = ".*%)/",
+  wordTrig = true,
+  regTrig = true,
+  name = "() frac",
+}, {
+  f(function(_, snip)
+    local match = vim.trim(snip.trigger)
+
+    local stripped = match:sub(1, #match - 1)
+
+    i = #stripped
+    local depth = 0
+    while true do
+      if stripped:sub(i, i) == ")" then
+        depth = depth + 1
+      end
+      if stripped:sub(i, i) == "(" then
+        depth = depth - 1
+      end
+      if depth == 0 then
+        break
+      end
+      i = i - 1
+    end
+
+    local rv = string.format(
+      "%s\\frac{%s}",
+      stripped:sub(1, i - 1),
+      stripped:sub(i + 1, #stripped - 1)
+    )
+    return rv
+  end, {}),
+  t("{"),
+  i(1),
+  t("}"),
+  i(0),
+})
+
 local math_wrA = {
+  frac,
+
   s({
     trig = "([%a])(%d)",
     regTrig = true,
