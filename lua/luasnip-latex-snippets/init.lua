a73a3b12566d54cd6efcfc6be69d739866175ee3
Iuri Mateus <20242530+iurimateus@users.noreply.github.com>
2022-03-22 08:54:41 -0300

    chore: conform to LuaSnip breaking changes

--- lua/luasnip-latex-snippets/init.lua
+++ lua/luasnip-latex-snippets/init.lua
@@ -12,75 +12,73 @@ local M = {}
 M.init = function()
   ls.config.setup({ enable_autosnippets = true })
 
-  ls.snippets = {
-    tex = {
-      ls.parser.parse_snippet(
-        { trig = "pac", name = "Package" },
-        "\\usepackage[${1:options}]{${2:package}}$0"
-      ),
+  ls.add_snippets("tex", {
+    ls.parser.parse_snippet(
+      { trig = "pac", name = "Package" },
+      "\\usepackage[${1:options}]{${2:package}}$0"
+    ),
 
-      -- ls.parser.parse_snippet({ trig = "nn", name = "Tikz node" }, {
-      --   "$0",
-      --   -- "\\node[$5] (${1/[^0-9a-zA-Z]//g}${2}) ${3:at (${4:0,0}) }{$${1}$};",
-      --   "\\node[$5] (${1}${2}) ${3:at (${4:0,0}) }{$${1}$};",
-      -- }),
-    },
-  }
+    -- ls.parser.parse_snippet({ trig = "nn", name = "Tikz node" }, {
+    --   "$0",
+    --   -- "\\node[$5] (${1/[^0-9a-zA-Z]//g}${2}) ${3:at (${4:0,0}) }{$${1}$};",
+    --   "\\node[$5] (${1}${2}) ${3:at (${4:0,0}) }{$${1}$};",
+    -- }),
+  })
 
   for _, snip in ipairs(require("luasnip-latex-snippets/math_i")) do
     snip.condition = pipe({ is_math })
     snip.wordTrig = false
-    table.insert(ls.snippets.tex, snip)
+    ls.add_snippets("tex", { snip })
   end
 
-  ls.autosnippets = {
-    tex = {},
-  }
+  local autosnippets = {}
 
   for _, snip in ipairs(require("luasnip-latex-snippets/math_wRA_no_backslash")) do
     snip.regTrig = true
     snip.condition = pipe({ is_math, no_backslash })
-    table.insert(ls.autosnippets.tex, snip)
+    table.insert(autosnippets, snip)
   end
 
   for _, snip in ipairs(require("luasnip-latex-snippets/math_rA_no_backslash")) do
     snip.wordTrig = false
     snip.regTrig = true
     snip.condition = pipe({ is_math, no_backslash })
-    table.insert(ls.autosnippets.tex, snip)
+    table.insert(autosnippets, snip)
   end
 
   for _, snip in ipairs(require("luasnip-latex-snippets/normal_wA")) do
     snip.condition = pipe({ not_math })
-    table.insert(ls.autosnippets.tex, snip)
+    table.insert(autosnippets, snip)
   end
 
   for _, snip in ipairs(require("luasnip-latex-snippets/math_wrA")) do
     snip.regTrig = true
     snip.condition = pipe({ is_math })
-    table.insert(ls.autosnippets.tex, snip)
+    table.insert(autosnippets, snip)
   end
 
   for _, snip in ipairs(require("luasnip-latex-snippets/math_wA_no_backslash")) do
     snip.condition = pipe({ is_math, no_backslash })
-    table.insert(ls.autosnippets.tex, snip)
+    table.insert(autosnippets, snip)
   end
 
   for _, snip in ipairs(require("luasnip-latex-snippets/math_iA")) do
     snip.wordTrig = false
     snip.condition = pipe({ is_math, no_backslash })
-    table.insert(ls.autosnippets.tex, snip)
+    table.insert(autosnippets, snip)
   end
 
   for _, snip in ipairs(require("luasnip-latex-snippets/math_bwA")) do
     snip.condition = pipe({ conds.line_begin, is_math })
-    table.insert(ls.autosnippets.tex, snip)
+    table.insert(autosnippets, snip)
   end
 
   for _, snip in ipairs(require("luasnip-latex-snippets/bwA")) do
     snip.condition = pipe({ conds.line_begin, not_math })
-    table.insert(ls.autosnippets.tex, snip)
+    table.insert(autosnippets, snip)
   end
+
+  ls.add_snippets("tex", autosnippets, { type = "autosnippets" })
 
   -- "Basic template": {
   --     "body": [
