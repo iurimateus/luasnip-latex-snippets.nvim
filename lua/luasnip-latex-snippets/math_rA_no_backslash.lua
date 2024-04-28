152f203f0d423e277af7a8fabefb2cd00b94cbe6
Iuri Mateus <20242530+iurimateus@users.noreply.github.com>
2024-04-28 06:16:09 -0300

    fix(math_rA_no_backslash): missing \psi
    Note that `wordTrig` must be true, otherwise it'd expand when typing
    epsilon.
    
    closes #36

--- lua/luasnip-latex-snippets/math_rA_no_backslash.lua
+++ lua/luasnip-latex-snippets/math_rA_no_backslash.lua
@@ -30,7 +30,7 @@ end
 
 local greek_postfix_completions = function()
   local re =
-    "[aA]lpha|[bB]eta|[cC]hi|[dD]elta|[eE]psilon|[gG]amma|[iI]ota|[kK]appa|[lL]ambda|[mM]u|[nN]u|[oO]mega|[pP]hi|[pP]i|[rR]ho|[sS]igma|[tT]au|[tT]heta|[zZ]eta|[eE]ta"
+    "[aA]lpha|[bB]eta|[cC]hi|[dD]elta|[eE]psilon|[gG]amma|[iI]ota|[kK]appa|[lL]ambda|[mM]u|[nN]u|[oO]mega|[pP]hi|[pP]i|[pP]si|[rR]ho|[sS]igma|[tT]au|[tT]heta|[zZ]eta|[eE]ta"
 
   local build = build_with_priority(postfix_trig, postfix_node, 200)
   return vim.tbl_map(build, vim.split(re, "|"))
@@ -51,7 +51,7 @@ function M.retrieve(is_math)
   local no_backslash = utils.no_backslash
 
   M.decorator = {
-    wordTrig = false,
+    wordTrig = true,
     trigEngine = "pattern",
     condition = pipe({ is_math, no_backslash }),
   }
