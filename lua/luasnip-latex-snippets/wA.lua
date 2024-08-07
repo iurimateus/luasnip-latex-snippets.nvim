6f42884398facfdeae76945e7854b531c9f7fc86
Carl Luo <91815967+erdaifuu@users.noreply.github.com>
2024-08-06 19:12:03 -0700

    Update wA.lua
    Added "mm" for Tex math mode in here

--- lua/luasnip-latex-snippets/wA.lua
+++ lua/luasnip-latex-snippets/wA.lua
@@ -12,6 +12,7 @@ function M.retrieve(not_math)
   return {
     parse_snippet({ trig = "mk", name = "Math" }, "\\( ${1:${TM_SELECTED_TEXT}} \\)$0"),
     parse_snippet({ trig = "dm", name = "Block Math" }, "\\[\n\t${1:${TM_SELECTED_TEXT}}\n.\\] $0"),
+    parse_snippet({ trig = "mm", name = "Tex Math" }, "$ ${1:${TM_SELECTED_TEXT}} $0"),
   }
 end
 
