cab134611eb755abe9ba95f5d86969f5cece448d
Iuri Mateus <20242530+iurimateus@users.noreply.github.com>
2024-11-22 09:44:58 -0300

    fix(util/ts_utils.lua): use vim.treesitter.get_node
    closes #5

--- lua/luasnip-latex-snippets/util/ts_utils.lua
+++ lua/luasnip-latex-snippets/util/ts_utils.lua
@@ -12,27 +12,8 @@ local TEXT_NODES = {
   label_reference = true,
 }
 
-local function get_node_at_cursor()
-  local pos = vim.api.nvim_win_get_cursor(0)
-  -- Subtract one to account for 1-based row indexing in nvim_win_get_cursor
-  local row, col = pos[1] - 1, pos[2]
-
-  local parser = vim.treesitter.get_parser(0, "latex")
-  if not parser then
-    return
-  end
-
-  local root_tree = parser:parse({ row, col, row, col })[1]
-  local root = root_tree and root_tree:root()
-  if not root then
-    return
-  end
-
-  return root:named_descendant_for_range(row, col, row, col)
-end
-
 function M.in_text(check_parent)
-  local node = get_node_at_cursor()
+  local node = vim.treesitter.get_node({ ignore_injections = false })
   while node do
     if node:type() == "text_mode" then
       if check_parent then
@@ -53,7 +34,7 @@ function M.in_text(check_parent)
 end
 
 function M.in_mathzone()
-  local node = get_node_at_cursor()
+  local node = vim.treesitter.get_node({ ignore_injections = false })
   while node do
     if TEXT_NODES[node:type()] then
       return false
